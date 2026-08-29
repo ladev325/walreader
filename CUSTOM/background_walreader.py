bcb67204cec4ca0f4d79d72e0fe6cc8347f6518a
ladev325 <vlad2008artemov@gmail.com>
2026-08-29 12:51:09 +0300

    Rewritten to use http get instead of websockets

--- CUSTOM/background_walreader.py
+++ CUSTOM/background_walreader.py
@@ -1,46 +1,40 @@
-import asyncio
-import websockets
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
+import threading
+import time
 
 COLOR_FILE = Path.home() / ".cache" / "wal" / "walreader.json"
 PORT = 6767
 
-current_websocket = None
-prev_data = None
+data = None
 
-async def handler(websocket):
-    global current_websocket, prev_data
-    if current_websocket is not None:
-        await current_websocket.close()
-        print('[Wal Reader] Another client is trying to connect, dropped previous connection')
-    
-    current_websocket = websocket
-    print('[Wal Reader] Client connected')
+class Handler(BaseHTTPRequestHandler):
+    def log_message(self, format, *args): return
+    def do_GET(self):
+        global data
+        if data is None:
+            self.send_response(503)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            print('[Wal Reader] 🕆 Sent: 503, data = None')
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(data.encode("utf-8"))
+            print(f'[Wal Reader] Sent: \n{data}')
 
-    prev_data = None
-    try:
-        while True:
-            try:
-                with open(COLOR_FILE, 'r', encoding='utf-8') as file:
-                    data = file.read()
-                    if prev_data != data:
-                        prev_data = data
-                        await websocket.send(data)
-                        print(f'[Wal Reader] Sent: \n{data}')
-            except FileNotFoundError:
-                print('[Wal Reader] 🕆 Color file not found') 
-            await asyncio.sleep(2)
-    except websockets.ConnectionClosed:
-        pass
-    finally:
-        if current_websocket is websocket:
-            current_websocket = None
-            prev_data = None
-        print('[Wal Reader] 🕆 Client disconnected')
+def check_file():
+    global data
+    while True:
+        try:
+            with open(COLOR_FILE, 'r', encoding='utf-8') as file:
+                data = file.read()
+        except FileNotFoundError:
+            print('[Wal Reader] 🕆 Color file not found')
+        time.sleep(1)
 
-async def main():
-    async with websockets.serve(handler, 'localhost', PORT):
-        print(f'[Wal Reader] Server is running on ws://localhost:{PORT}, waiting for client')
-        await asyncio.Future()
-
-asyncio.run(main())
+print(f'[Wal Reader] The server is running on http://localhost:{PORT}')
+threading.Thread(target=check_file, daemon=True).start()
+HTTPServer(("localhost", PORT), Handler).serve_forever()
